01fae5f3bc544cb68c1ad12efaebc77db7f874b2
Henry Liao <hliao62@gatech.edu>
2022-12-14 23:38:03 -0800

    Title Page Transitions

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,38 +1,46 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import {Fade} from 'react-awesome-reveal'
-import '../styles/Container.css'
+
 import '../styles/Title.css'
+import '../styles/Container.css'
+import '../styles/App.css'
 
 
 export default function Title(props){
     return (
-        <section id="title" >
-            
-            <Fade left={true} bottom={false} duration={1000} delay={500} distance="30px">
-                <div class="container-row">
-                    <div class ="row-item">
-                        <h1>Hi, I'm Henry.</h1>
-                        <h3>Software Engineer | Roboticist </h3>
-
-
-                        <p class="hero-cta load-hidden">
-                        <a rel="noreferrer" class="cta-btn cta-btn--hero" href="#about">
-                                Get to know me.
-                            </a>
-                        </p>
+        <div class="screen-container">
+            <section id="title" >            
+                <div id = "title-container" class="container-row" >
+                    <div class ="row-item-title">
+                        <Fade left={true} bottom={false} duration={1000} delay={500} distance="30px">
+                            <div class ="row-item-title">
+                                <h1>Hi, I'm Henry.</h1>
+                                <h2>Software Engineer | Roboticist </h2>
+                            </div>
+                        </Fade>
                     </div>
-
-                    <div class ="row-item">
-                        <img class="circular" src="https:\\s-hliao.github.io\portfolio-react\My Picture.jpg" alt="Profile Pic" />
+                    
+                    
+                        <div class ="row-item-img">
+                        <Fade left={true} bottom={false} duration={1000} delay={1000} distance="30px">
+                    <div class ="row-item-img">
+                       
+                            <img class="circular" src="https:\\s-hliao.github.io\portfolio-react\My Picture.jpg" alt="Profile Pic" />
+                       
                     </div>
+                    </Fade>
+                    </div>
+                    
                 </div>
                         
                     
 
-            </Fade>
+            
 
 
-        </section>
+            </section>
+        </div>
+       
     );
 }
